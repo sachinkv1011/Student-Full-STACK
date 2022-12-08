6662939f5b2c3dc8b5787c908780b17298e8478d
SACHIN K V <sachin.kv.1011@gmail.com>
2022-12-08 20:32:33 +0530

    view

---
+++ src/app/view-all/view-all.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-all',
+  templateUrl: './view-all.component.html',
+  styleUrls: ['./view-all.component.css']
+})
+export class ViewAllComponent {
+
+  students:any=[]
+
+}
